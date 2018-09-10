a20cbf83ba38e7b30f53b2ffec2b61dbb56e6307
QinChBeSt <531239502@qq.com>
2018-09-10 16:30:06 +0800

    update

--- takeOut/Login/RegisterVC.m
+++ takeOut/Login/RegisterVC.m
@@ -23,7 +23,7 @@
 @end
 
 @implementation RegisterVC
-
+//svnnnnnnnnsdsadasdas
 - (void)viewDidLoad {
     [super viewDidLoad];
     UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREENH_HEIGHT)];
